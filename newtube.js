a2ae6dcc23dd2a21b143bbb0d892a89fd7820fb6
flyrev <flyrev@Stargazer.(none)>
2013-01-13 13:04:40 +0100

    WOO

---
+++ newtube.js
@@ -0,0 +1,26 @@
+﻿function changeHandler(){
+alert("ding");
+   //Do Something...maybe another function showAlert(), for instance
+   if(document.getElementById("seen_checkbox".checked)) {
+      alert("checked");
+   }
+   else{
+      alert("unchecked");
+   }
+}
+
+document.getElementById("watch-headline-title").innerHTML += "flyrev";
+document.getElementById("watch-headline-title").innerHTML += ":)";
+
+var input = document.createElement("have_seen_it");
+input.type = "checkbox";
+
+var cb = document.createElement( "input" );
+cb.type = "checkbox";
+cb.id = "seen_checkbox";
+cb.checked = false; // WELL THAT DEPENDS DOESN'T IT.
+var text = document.createTextNode( "checkbox" );
+document.getElementById( 'watch7-headline' ).appendChild( text );
+document.getElementById( 'watch7-headline' ).appendChild( cb );
+
+document.getElementById("seen_checkbox").addEventListener('change', 'changeHandler');
